92c36d5ceda1ddd08ca6bc51a766f8d34d96c4b8
Bryan Jay Zacarias <zacarbr@oocl.com>
2022-09-13 10:20:09 +0800

    refactor: fixed lines spacing

--- src/App.js
+++ src/App.js
@@ -6,11 +6,31 @@ function App() {
   return (
     <div className='App'>
       <h1>My favorite thing to do is {favorite}!</h1>
-      <button className='button' onClick={()=>setFavorite("reading")}>Reading</button>
-      <button className='button' onClick={()=>setFavorite("singing")}>Singing</button>
-      <button className='button' onClick={()=>setFavorite("dancing")}>Dancing</button>
-      <button className='button' onClick={()=>setFavorite("watching movies")}>Watching Movies</button>
-      
+      <button 
+        className='button' 
+        onClick={()=>setFavorite("reading")}
+      >
+        Reading
+      </button>
+      <button 
+        className='button' 
+        onClick={()=>setFavorite("singing")}
+      >
+        Singing
+      </button>
+      <button 
+        className='button' 
+        onClick={()=>setFavorite("dancing")}
+      >
+        Dancing
+      </button>
+      <button 
+        className='button' 
+        onClick={
+          ()=>setFavorite("watching movies")}
+      >
+        Watching Movies
+      </button>
     </div>
   );
 }
